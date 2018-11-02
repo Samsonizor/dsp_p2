d72fc64963574648e6a0cd4146c86e58570709ca
Samsonizor <samsonizor@gmail.com>
2018-11-02 12:42:28 -0400

    implemented "pointers" for better readability

--- matlab/overlap_save.m
+++ matlab/overlap_save.m
@@ -3,12 +3,16 @@ function [ y ] = overlap_save( h, x, N )
     k = 0;
     h_transform = fft([h zeros(1, N-length(h))]);
     M = length(h);
-    while((N-M)*k+N < length(x))
-        x_transform = fft(x((N-M)*k+1:(N-M)*k+N));
+    x_pointer = 1;
+    y_pointer = M+1;
+    while(x_pointer+N-1 <= length(x))
+        x_transform = fft(x(x_pointer:x_pointer+N-1));
         y_transform = h_transform.*x_transform;
         y_i = ifft(y_transform);
-        y(M+k*(N-M)+1:(N-M)*k+N) = y_i(M+1:end);
+        y(y_pointer:y_pointer+(N-M)-1) = y_i(M+1:end);
         k = k+1;
+        x_pointer = x_pointer + (N-M);
+        y_pointer = y_pointer + (N-M);
     end
 end
 
