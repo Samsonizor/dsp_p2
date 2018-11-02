0f40159695956b30bc863030159f42186e9d7439
Samsonizor <samsonizor@gmail.com>
2018-11-02 13:08:58 -0400

    overlap_save now handles the last block of input

--- matlab/overlap_save.m
+++ matlab/overlap_save.m
@@ -1,8 +1,8 @@
 function [ y ] = overlap_save( h, x, N )
+    M = length(h);
     y = zeros(1, length(x));
     k = 0;
     h_transform = fft([h zeros(1, N-length(h))]);
-    M = length(h);
     x_pointer = 1;
     y_pointer = M+1;
     while(x_pointer+N-1 <= length(x))
@@ -13,6 +13,14 @@ function [ y ] = overlap_save( h, x, N )
         k = k+1;
         x_pointer = x_pointer + (N-M);
         y_pointer = y_pointer + (N-M);
+    end
+    if(x_pointer >= length(x))
+        return
+    else
+        x_transform = fft([x(x_pointer:end) zeros(1, N-(length(x)-x_pointer)-1)]);
+        y_transform = h_transform.*x_transform;
+        y_i = ifft(y_transform);
+        y(y_pointer:y_pointer+(N-M)-1) = y_i(M+1:end);
     end
 end
 
