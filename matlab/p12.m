34b6b4d7e5ac14d8e92f0d870d58b1a9f97cbe55
Samsonizor <samsonizor@gmail.com>
2018-11-06 10:17:43 -0500

    confirmation of the correct H!

--- matlab/p12.m
+++ matlab/p12.m
@@ -8,11 +8,20 @@ s_iter = zeros(1,length(delta));
 c = [0 0 -.11];
 k = [-.53 .91];
 
+c0 = c(1);
+c1 = c(2);
+c2 = c(3);
+k1 = k(1);
+k2 = k(2);
+
+a = [1 k1 + k1*k2 k2];
+b = [c2 + c0*k2 + c1*k1 + c2*k1 + c2*k2 + c1*k1*k2 + c2*k1*k2, c1 + c0*k1 + c1*k2 + c0*k1*k2, c0];
+
 % c = [1 1 1];
 % k = [1 1];
 [y, s] = proj2sfg_iterate(c, k, [0; 0], delta);
 
-[y_iter, s_iter] = proj2sfg_statespace_iterate(c, k, [0; 0], delta);
+[y_iter] = fun_iterate(b, a, delta, [0 0 0 0]);
 plot(n, y_iter)
 hold on
 plot(n, y)
